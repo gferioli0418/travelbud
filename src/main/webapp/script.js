aff9cc8c9e80658742e4c45163f2da4bb4993cc2
NzingaHolloman <you@example.com>
2020-04-02 17:48:33 -0400

    "Add searchResults pages example"

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -2,12 +2,30 @@
  * Adds a random location to the page.
  */
 function addRandomLocation() {
-  const locations = ['Canada', 'Mexico', 'US'];
+  const locations = ['Canada', 'Mexico', 'US']
 
-  // Pick a random location.
-  const location = locations[Math.floor(Math.random() * locations.length)];
+      // Pick a random location.
+      const location = locations[Math.floor(Math.random() * locations.length)]
+
+      // Add it to the page.
+      const locationContainer = document.getElementById('location-container')
+  locationContainer.innerText = location
+}
+
+function addRandomGreeting() {
+  const greetings = [
+    "Brazil!",
+    "Bangkok, Thailand!",
+    "Accra, Ghana!",
+    "Botswana!",
+    "Machu Picchu!",
+    "Beijing, China!",
+    "Chichen Itza, Mexico!",
+  ];
+  // Pick a random greeting.
+  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
   // Add it to the page.
-  const locationContainer = document.getElementById('location-container');
-  locationContainer.innerText = location;
+  const greetingContainer = document.getElementById("greeting-container");
+  greetingContainer.innerText = greeting;
 }
